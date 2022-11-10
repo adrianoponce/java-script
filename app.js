da2d0d1970dd16bcee7f7fb4731fc8671e6e223e
Adriano Cesare Ponce <112665585+adrianoponce@users.noreply.github.com>
2022-11-09 21:51:01 -0300

    Resumo do Javascript.

---
+++ app.js
@@ -0,0 +1,59 @@
+console.log("Olá mundo")
+console.log(2+2)
+console.log(10*2)
+console.log(30+1)
+console.log(100/2)
+console.log(35.7*3.8)
+console.log(99+21+125)
+console.log(32-[3/4.5])
+console.log(4+4+4+[4/4])
+console.log(127.75)
+/*comentário varias linhas*/
+// comentário de uma linha
+/* VARIÁVEL
+- espaço reservado na memoria para guardar um valor
+- toda variável tem um NOME e um TIPO
+- O NOME dve ser simples e significativo
+- sem caracter especial, sem espaço
+- diferencia maiúsculas de minúsculas
+- Declaração de variáveis*/
+let nome = 'Adriano'
+let idade = 36
+let nota1 = 7.5
+let fumante = false
+console.log(idade)
+console.log(idade, nota1,fumante)
+console.log('Sua nota é ' + nota1)
+/*TIPO*/
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof nota1)
+console.log(typeof fumante)
+let veiculos = ['carro','moto','avião']
+console.log(veiculos)
+console.log(typeof veiculos)
+let cliente = {
+    nome: 'Adriano',
+    idade: 36
+}
+console.log(cliente)
+console.log(typeof cliente)
+let salvar = function(){
+
+}
+console.log(typeof salvar)
+/*OPERADORES RELACIONAIS*/
+console.log(10 > 3)
+console.log(5 < 3)
+console.log(15 >= 10)
+console.log(15 <= 10)
+console.log(10 == 10)
+console.log(10 != 3)
+/*OPERADORES LÓGICOS*/
+console.log(10>3 && 10>5) // AND
+console.log(10>3 || 10>5) // OR
+console.log(!10>3) // NOT inverte o resultado para false
+
+
+
+
